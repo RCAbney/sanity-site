687946e15227fa886d8c6b30ee3dea275351d6a8
RCAbney <ryan@rcabney.com>
2024-02-23 21:26:25 -0500

    add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import { sanityIntegration } from "@sanity/astro";
 
 export default defineConfig({
+  prefetch: true,
   integrations: [sanityIntegration({
     projectId: '90jrxj4o',
     dataset: 'production',
